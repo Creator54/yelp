204378e7f34ca864ede31ad957fa3c31686700a5
Bisheswar Mahato <42653296+Vishes77@users.noreply.github.com>
2021-10-29 14:24:49 +0530

    Database Connected

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express =  require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const campground = require('./models/campgrounds');
 
+mongoose.connect('mongodb://localhost:27017/yelp-camp',{
+    useNewUrlParser : true,
+    useUnifiedTopology:true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console,"connection Error"))
+db.once('open',()=>{
+    console.log("Database connceted");
+})
+
+const app = express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
 
 app.get('/',(req,res)=>{
-    res.render('index');
+    res.render('home');
+})
+
+app.get('/makecampgrounds', async (req,res)=>{
+    const newcampground = new campground({tittle : 'FirstCampground',description:'This is the First One.'});
+    await newcampground.save();
+    res.send(newcampground);
 })
 
 app.listen(3000,()=>{
---
+++ models/campgrounds.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CampgroundsSchema = new Schema({
+    tittle : String,
+    prince : String,
+    description : String,
+    location :String
+})
+
+
+module.exports = mongoose.model('Campground',CampgroundsSchema);
