d6da7ad05cb813f81ba7ffc426eb9fc0e3d651cd
Bisheswar Mahato <42653296+Vishes77@users.noreply.github.com>
2021-11-07 01:43:23 +0530

    error.ejs templete added , a page to just error problems.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const campground = require('./models/campgrounds');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewUrlParser : true,
@@ -58,6 +59,8 @@ app.get('/campgrounds/new',(req,res)=>{
 
 app.post('/campgrounds', catchAsync(async(req,res,next)=>{  // Basic Custom erroe
         // console.log(req.body.campground);
+        if(!req.body.campground) throw new ExpressError('Invalid Campground',404); // throw to CatchAsync 
+
         const camp = new campground(req.body.campground);
         await camp.save();
         res.redirect(`/campgrounds/${camp._id}`);
@@ -88,9 +91,17 @@ app.delete('/campgrounds/:id',  catchAsync(async(req,res)=>{
     res.redirect('/campgrounds');
 }))
 
+app.all('*',(req,res,next)=>{
+    next(new ExpressError('Page Not Found',404));
+})
+
+
 //Basic custom Error Handler Added.
 app.use((err,req,res,next)=>{
-    res.send('Basic Error Handler Working');
+    const {statusCode = 500 } = err;
+    if(!err.message) err.message = 'Something Went Wrong';
+    res.status(statusCode).render('error', { err } );
+    // res.send('Basic Error Handler Working');
 })
 
 app.listen(3000,()=>{
