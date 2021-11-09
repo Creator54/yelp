2d83be1dfb8ff3aeef832f4500339fefb83b0756
Bisheswar Mahato <42653296+Vishes77@users.noreply.github.com>
2021-11-10 03:27:11 +0530

    return to Behavior

--- routes/users.js
+++ routes/users.js
@@ -32,7 +32,9 @@ router.post('/register' ,catchAsync(async(req,res)=>{
 
 router.post('/login', passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}),(req,res)=>{
     req.flash('success','Welcome Bcak');
-    res.redirect('/campgrounds'); // Demmo account {username : abc , password : password }
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);// Demmo account {username : abc , password : password }
 })
 
 router.get('/logout',(req,res)=>{
