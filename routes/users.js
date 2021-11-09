899bf5d820118764fd3d76222981738256071e1f
Bisheswar Mahato <42653296+Vishes77@users.noreply.github.com>
2021-11-10 03:20:36 +0530

    Fixing Register route,

--- routes/users.js
+++ routes/users.js
@@ -18,12 +18,15 @@ router.post('/register' ,catchAsync(async(req,res)=>{
         const {email,username,password} = req.body;
         const user = new User({email,username});
         const registeredUser = await User.register(user,password);
+        req.login(registeredUser , err =>{
+            if(err) return next(err);
+            req.flash('success','You are Registered as a Member');
+            res.redirect('/campgrounds');
+        })
         // console.log(registeredUser);
-        req.flash('success','You are Registered as a Member');
-        res.redirect('/campgrounds');
     }catch(e){
         req.flash('error',e.message);
-        res.redirect('/register');
+        res.redirect('register');
     }
 }));
 
