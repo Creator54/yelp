d180e5b18d1c5d6744f5bb00707d4cd243496745
Bisheswar Mahato <42653296+Vishes77@users.noreply.github.com>
2021-11-12 17:33:50 +0530

    Mapbox Section Done.

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -5,5 +5,17 @@ const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
     center: campground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 9 // starting zoom    
+    zoom: 10 // starting zoom    
 });
+
+//now for adding marker.
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${campground.tittle}</h3><p>${campground.location}</p>`
+            )
+    )
+    .addTo(map)
